92fd7d29ad3598d49761fe4e175c6eda11fa55a6
Rong <rongster3544@gmail.com>
2020-02-20 15:13:40 -0800

    Table using hook

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,74 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { reverseDirection, sortData } from '../../utils';
 import styles from './Table.css';
 
-export default class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sortTag: '',
-      sortDirection: '',
-    };
-  }
+const Table = ({ data }) => {
+  const [sortTag, setSortTag] = useState('');
+  const [sortDirection, setSortDirection] = useState('');
 
-  handleSortClick = (e) => {
+  const handleSortClick = (e) => {
     const { id } = e.target;
-    this.setState((state) => {
-      if (state.sortTag !== id) {
-        return {
-          sortTag: id,
-          sortDirection: 'up',
-        };
-      }
-      return {
-        sortDirection: reverseDirection(state.sortDirection),
-      };
-    });
-  }
+    if (sortTag !== id) {
+      setSortTag(id);
+      setSortDirection('up');
+    } else {
+      setSortDirection(reverseDirection(sortDirection));
+    }
+  };
 
-  renderTableHeader = () => {
-    const { data } = this.props;
-    const { sortDirection, sortTag } = this.state;
+  const renderTableHeader = () => {
     const header = Object.keys(data[0]);
     return header.map((key) => (
       <th
         id={key}
-        onClick={this.handleSortClick}
+        onClick={handleSortClick}
       >
         {key === sortTag && sortDirection === 'up' && <span>&uarr;</span>}
         {key === sortTag && sortDirection === 'down' && <span>&darr;</span>}
         {key.toUpperCase()}
       </th>
     ));
-  }
+  };
 
-  renderTableData = () => {
-    const { data } = this.props;
-    return data.map((element) => {
-      const { tag, content } = element;
-      return (
-        <tr>
-          <td>{tag}</td>
-          <td>{content}</td>
-        </tr>
-      );
-    });
-  }
-
-  render() {
-    const { data } = this.props;
-    const { sortTag, sortDirection } = this.state;
-    // mutating props here, but should be okay.
-    sortData(data, sortTag, sortDirection);
+  const renderTableData = () => data.map((element) => {
+    const { tag, content } = element;
     return (
-      <div>
-        <table className={styles.table}>
-          <tbody>
-            <tr>{this.renderTableHeader()}</tr>
-            {this.renderTableData()}
-          </tbody>
-        </table>
-      </div>
+      <tr>
+        <td>{tag}</td>
+        <td>{content}</td>
+      </tr>
     );
-  }
-}
+  });
+
+  // mutating props here, but should be okay.
+  sortData(data, sortTag, sortDirection);
+  return (
+    <div>
+      <table className={styles.table}>
+        <tbody>
+          <tr>{renderTableHeader()}</tr>
+          {renderTableData()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
