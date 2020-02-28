01b53a0f98083b12db0dc1ecf599c0dfde7f6a9b
Rong <rongster3544@gmail.com>
2020-02-27 17:39:40 -0800

    App changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from './Components/Table/Table';
 import Checkbox from './Components/Checkbox/Checkbox';
 import { filterData } from './utils';
 import styles from './App.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      checkedTags: [],
-    };
-  }
+const App = () => {
+  const [data, setData] = useState([]);
+  const [checkedTags, setCheckedTags] = useState([]);
 
-  async componentDidMount() {
-    const data = await fetch('/api/data');
-    const json = await data.json();
-    this.setState({
-      data: json,
-    });
-  }
+  useEffect(async () => {
+    const resp = await fetch('/api/data');
+    const json = await resp.json();
+    setData(json);
+  }, []);
 
-  handleCheckboxClick = (e) => {
+  const handleCheckboxClick = (e) => {
     const { name, checked } = e.target;
     if (checked) {
-      this.setState((state) => ({
-        checkedTags: [...state.checkedTags, name],
-      }));
+      setCheckedTags([...checkedTags, name]);
     } else {
-      this.setState((state) => {
-        const updatedTags = state.checkedTags.filter((tag) => tag !== name);
-        return { checkedTags: updatedTags };
-      });
+      const updatedTags = checkedTags.filter((tag) => tag !== name);
+      setCheckedTags(updatedTags);
     }
-  }
+  };
 
-  render() {
-    const {
-      data,
-      checkedTags,
-    } = this.state;
-    const filteredData = filterData(data, checkedTags);
+  const filteredData = filterData(data, checkedTags);
 
-    return (
-      <>
-        <div className={styles.title}>
-          <img src='/assets/logo.png' alt='Rick and Morty' />
-        </div>
-        <Checkbox label='character' onClick={this.handleCheckboxClick} />
-        <Checkbox label='location' onClick={this.handleCheckboxClick} />
-        <Checkbox label='quote' onClick={this.handleCheckboxClick} />
-        {data.length !== 0 && (
-          <Table data={filteredData} />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className={styles.title}>
+        <img src='/assets/logo.png' alt='Rick and Morty' />
+      </div>
+      <Checkbox label='character' onClick={handleCheckboxClick} />
+      <Checkbox label='location' onClick={handleCheckboxClick} />
+      <Checkbox label='quote' onClick={handleCheckboxClick} />
+      {data.length !== 0 && (
+        <Table data={filteredData} />
+      )}
+    </>
+  );
+};
+
+export default App;
